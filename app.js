57712c19e70d75f9678dd7a615f214568ccfe0a2
MattiHel85 <70198997+MattiHel85@users.noreply.github.com>
2021-02-16 14:42:54 +0200

    Add cabin model and connect to Mongo

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
-const PORT = 3000;
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Cabin = require('./models/cabin');
+const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/cabin-Finland', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log('DB Connected!')
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,6 +24,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     console.log('Hello World!');
     res.render('home');
+});
+
+app.get('/makecabin', async (req, res) => {
+    const cabin = new Cabin({ title: 'Helvetin Silmä', price:'€666'});
+    await cabin.save();
+    res.send(cabin);
 });
 
 
